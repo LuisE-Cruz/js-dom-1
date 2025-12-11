3dbe14c37fa72e4293324967fc06a3714904a82d
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-11 17:10:27 +0100

    aggiunta di evento "click" sul bottone che fa da toggle d-none alle immagini

--- js/script.js
+++ js/script.js
@@ -5,7 +5,14 @@ const off = document.getElementById("lamp-off")
 const turnOn = document.getElementById("button-on")
 const turnOff = document.getElementById("button-off")
 
-turnOn.addEventListener("click"{
+
+//Aggiunto evento al click del bottone 
+
+turnOn.addEventListener("click", () => {
     
+    off.classList.toggle("d-none");
+    on.classList.toggle("d-none");
+    turnOff.classList.toggle("d-none");
+    turnOn.classList.toggle("d-none");
 }
-)
+);
