4dbcb5375a086adf21c9864f7029b66cedb23317
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-11 17:01:06 +0100

    Dichiarato gli elementi su javascript grazie a getElemtById

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Dichiaro i diversi elementi
+
+const on = document.getElementById("lamp-on")
+const off = document.getElementById("lamp-off")
+const turnOn = document.getElementById("button-on")
+const turnOff = document.getElementById("button-off")
+
+turnOn.addEventListener("click"{
+    
+}
+)
