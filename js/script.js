a73ec6bb5c9bda2b46c7a8a67bc60d2119147ff8
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-11 17:41:24 +0100

    Aggiustamenti, fine progetto

--- js/script.js
+++ js/script.js
@@ -3,17 +3,15 @@
 const on = document.getElementById("lamp-on")
 const off = document.getElementById("lamp-off")
 const turnOn = document.getElementById("button-on")
-// const turnOff = document.getElementById("button-off")
 
 
 //Aggiunto evento al click del bottone 
 
 turnOn.addEventListener("click", () => {
-    
+
     off.classList.toggle("d-none");
     on.classList.toggle("d-none");
-    // turnOff.classList.toggle("d-none");
-    // turnOn.classList.toggle("d-none");
+
     turnOn.innerText = turnOn.innerText === "Accendi" ? "Spegni" : "Accendi"
 
 }
